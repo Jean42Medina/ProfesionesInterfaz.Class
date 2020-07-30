8f86544845302edeaef740955343bf234168b579
Jean42Medina <jeanmedina-1996@hotmail.com>
2020-07-30 11:48:52 -0500

    Agregacion de Class Carpintero

---
+++ Profesiones.Interface/Carpintero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Profesiones.Interface
+{
+    public class Carpintero
+    {
+        public string Diseñar()
+        {
+            return "Estoy diseñando";
+        }
+        public string Medir()
+        {
+            return "Estoy midiendo";
+        }
+        public string Serruchar()
+        {
+            return "Estoy serruchar";
+        }
+        public string Clavar()
+        {
+            return "Estoy clavando";
+        }
+        public string Lijar()
+        {
+            return "Estoy lijando";
+        }
+        public string Lacar()
+        {
+            return "Estoy lacando";
+        }
+
+    }
+}
